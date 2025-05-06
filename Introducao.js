528cbe8e769efec91dba7e85306823e3581e6dd0
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-06 01:20:37 -0300

    Metodos

--- Introducao.js
+++ Introducao.js
@@ -119,6 +119,28 @@ console.log(arrJogadores[1]+ 'tem' +arrPontos[1]+' pontos.');
         console.log(arrPrato);
         arrPrato.push('feijão');
         console.log(arrPrato);
+
+        // UNSHIFTH ---> ADD A FIRST POSITION
+        var arrEmenta = new Array('Matemática', 'Biologia', 'Português');
+        arrEmenta.unshift('Fisica');
+        console.log(arrEmenta);
+
+        // POP ---> REMOVE THE LAST POSITION
+        arrEmenta.pop();
+        console.log(arrEmenta)
+
+        // SHIFTH ---> REMOVE FIRST POSITION
+        var arrMedicos = new Array('Dr. Barcelos', 'Dra. Fernanda');
+        arrMedicos.shift();
+        console.log(arrMedicos);
+
+        // INDEXOF ---> RETURN POSITION TO VALUE
+        var arrPrato = new Array('Arroz','Feijao', 'Tomate', 'Carne');
+        arrPrato.push('Ovo');
+        if(arrPrato.indexOf('Arroz')!==-1){
+            arrPrato.shift();
+        }
+        console.log(arrPrato);
     
 
 
