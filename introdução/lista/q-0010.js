de9f34e3bf1aa9bd97f711b50e657e9ac1753df1
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-19 17:36:09 -0300

    feat: lendo documentação e corrigindo erros semânticos

--- introdução/lista/q-0010.js
+++ introdução/lista/q-0010.js
@@ -1,19 +1,22 @@
 class Aluno {
-    constructor() {
-        this.name = "Ranilton";
-        this.age = 18;
-        this.height = 1.78;
-        this.weight = 68.7;
+    constructor(name,age,height,weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
     }
-
+    ShowID() {
+        console.log("NAME: " + this.name);
+        console.log("IDADE: " + this.age);
+        console.log("ALTURA: " + this.height);
+        console.log("PESO: " + this.weight);
+    }
 }
 
-function ShowID() {
-    console.log("NAME: " + this.name);
-    console.log("IDADE: " + this.age);
-    console.log("ALTURA: " +this.height);
-    console.log("PESO: " +this.weight);
-}
+const Ranilton = new Aluno("Ranilton",18,1.70,68.5);
 
+const Ramon = new Aluno("Ramon",18,1.70,75.7);
 
-console.log(ShowID())
+Ranilton.ShowID();
+console.log("");
+Ramon.ShowID();
