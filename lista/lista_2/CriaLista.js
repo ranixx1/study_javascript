5f83b49efb6ae1df6de5ca41d7394c5342a29010
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-19 23:55:40 -0300

    feat: cria uma função para usar em outros arquivos

--- lista/lista_2/CriaLista.js
+++ lista/lista_2/CriaLista.js
@@ -1,11 +1,9 @@
-let dados = [];
-let quantidade = 5
+export function criarArrayAleatorio(quantidade) {
+    let dados = [];
 
-for (let i = 0; i < quantidade; i++) {
-    const numeroAleatorio = Math.floor(Math.random()*100)+1;
-    dados.push(numeroAleatorio);
-
+    for (let i = 0; i < quantidade; i++) {
+        const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+        dados.push(numeroAleatorio);
+    }
+    return dados;
 }
-
-console.log(dados);
-
