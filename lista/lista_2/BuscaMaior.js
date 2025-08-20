63d80c1c0c39b8b9de86b6f7c55958385d550dfd
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-19 23:54:47 -0300

    feat: adiciona import do criar lista aleátorio

--- lista/lista_2/BuscaMaior.js
+++ lista/lista_2/BuscaMaior.js
@@ -1,3 +1,4 @@
+import { criarArrayAleatorio } from "./CriaLista.js";
 function buscaMaior(arr){
     let maior = 0;
     let maior_indice =0;
@@ -11,10 +12,11 @@ function buscaMaior(arr){
     return maior_indice;
 }
 
-const lista = [3,4,3,4,3,43,4,3,4,3,432,43734,343,24,24,242424,2245];
+const MeuArr = criarArrayAleatorio(4);
 
-const Imaior = buscaMaior(lista);
-const Vmaior = lista[buscaMaior(lista)];
+const Imaior = buscaMaior(MeuArr);
+const Vmaior = MeuArr[buscaMaior(MeuArr)];
 
-console.log(Imaior);
-console.log(Vmaior);
+console.log(MeuArr)
+console.log("Maior indice: " + Imaior);
+console.log("Valor no indice " + Vmaior);
