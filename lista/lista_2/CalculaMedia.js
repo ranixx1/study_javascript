281bfc6ca29afaada226e391150a875d63625fc7
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-21 23:01:15 -0300

    comenta teste

--- lista/lista_2/CalculaMedia.js
+++ lista/lista_2/CalculaMedia.js
@@ -1,5 +1,5 @@
 import { geraNotasTest } from "./CriaListaNotas.js";
-function CalculaMedia(Arr) {
+export function CalculaMedia(Arr) {
     let valor_final = 0;
     for (let i = 0; i < Arr.length; i++) {
         valor_final += Arr[i]
@@ -7,7 +7,10 @@ function CalculaMedia(Arr) {
     let media = valor_final / Arr.length;
     return media;
 }
+
+/* Testes
 let gerar_lista = geraNotasTest(20);
 console.log("Notas:" + gerar_lista)
 let mediaNotas = CalculaMedia(gerar_lista);
 console.log("Média das notas:", mediaNotas);
+*/
