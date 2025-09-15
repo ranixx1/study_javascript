f01edd6c6ba5d35b81f648604fb925e624174fe7
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-09-15 10:05:23 -0300

    feat: adiciona ProgressBar

---
+++ src/utils/ProgressBar/main.js
@@ -0,0 +1,8 @@
+let progress = 0;
+
+function increaseProgress(){
+    if(progress <100){
+        progress+=10;
+        document.getElementById('progress-bar').style.width = progress + "%";
+    }
+}
