4b42f0fb8528e8e4c53ccf593ecf6adc62689344
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-11-21 23:28:45 -0300

    IsAdult? Retun true, else, false

---
+++ src/func/arrowF/ex1.js
@@ -0,0 +1,7 @@
+//Crie uma arrow function chamada isAdult que recebe uma idade e retorna true se a idade for 18 ou maior, e false caso contrário.
+
+const isAdult = (age) => age >= 18;
+
+const x = 10;
+console.log(isAdult(x))
+
