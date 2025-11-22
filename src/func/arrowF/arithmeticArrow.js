8827b85f5d08b395c6227de25713f42fde247848
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-11-21 23:10:52 -0300

    arrow functions introduction

---
+++ src/func/arrowF/arithmeticArrow.js
@@ -0,0 +1,13 @@
+const add = (a,b) => a + b;
+const sub = (a,b) => a - b;
+const mul = (a,b) => a * b;
+const div = (a,b) => a / b;
+
+
+const a = 9;
+const b = 3;
+
+console.log(add(a,b))
+console.log(sub(a,b))
+console.log(mul(a,b))
+console.log(div(a,b))
