311a861d68fa46239347b4b9da0200e6d543d944
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-01-20 21:59:19 -0300

    feat: separa função

---
+++ src/utils/CsvToJS.js
@@ -0,0 +1,10 @@
+export function jsToCsv(data) {
+    if (!data.length) return "";
+
+    const headers = Object.keys(data[0]).join(",");
+    const rows = data.map(obj =>
+        Object.values(obj).join(",")
+    );
+
+    return [headers, ...rows].join("\n");
+}
